dcb66a506a19bc397404d40278b67f1d8602f2b4
solji <solji.choi89@gmail.com>
2025-01-31 04:13:41 +0900

    PaginationType1 컴포넌트를 비지니스 컴포넌트로 승격

---
+++ frontend/src/lib/business/components/PaginationType1.tsx
@@ -0,0 +1,135 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+} from "@/components/ui/pagination";
+
+interface UsePaginationProps {
+  baseQueryString: string;
+  totalPages: number;
+  currentPageNumber: number;
+}
+
+function usePagination({
+  baseQueryString,
+  totalPages,
+  currentPageNumber,
+}: UsePaginationProps) {
+  const paginationArmSize = 1;
+
+  const pageButtonUrl = (pageNumber: number) =>
+    `?page=${pageNumber}&${baseQueryString}`;
+
+  const prevEllipsisButtonPageNumber =
+    currentPageNumber - paginationArmSize - 1 > 1
+      ? currentPageNumber - paginationArmSize - 1
+      : undefined;
+
+  const nextEllipsisButtonPageNumber =
+    currentPageNumber + paginationArmSize + 1 < totalPages
+      ? currentPageNumber + paginationArmSize + 1
+      : undefined;
+
+  const middlePages = Array.from(
+    { length: totalPages },
+    (_, i) => i + 1,
+  ).filter(
+    (pageNum) =>
+      pageNum > 1 &&
+      pageNum < totalPages &&
+      pageNum >= currentPageNumber - paginationArmSize &&
+      pageNum <= currentPageNumber + paginationArmSize,
+  );
+
+  return {
+    pageButtonUrl,
+    prevEllipsisButtonPageNumber,
+    nextEllipsisButtonPageNumber,
+    middlePages,
+  };
+}
+
+interface PaginationProps {
+  className?: string;
+  baseQueryString: string;
+  totalPages: number;
+  currentPageNumber: number;
+}
+
+export default function PaginationType1({
+  className,
+  baseQueryString,
+  totalPages,
+  currentPageNumber,
+}: PaginationProps) {
+  const {
+    pageButtonUrl,
+    prevEllipsisButtonPageNumber,
+    nextEllipsisButtonPageNumber,
+    middlePages,
+  } = usePagination({
+    baseQueryString,
+    totalPages,
+    currentPageNumber,
+  });
+
+  return (
+    <Pagination className={className}>
+      <PaginationContent>
+        <PaginationNumber
+          pageNumber={1}
+          currentPageNumber={currentPageNumber}
+          pageButtonUrl={pageButtonUrl}
+        />
+
+        {prevEllipsisButtonPageNumber && (
+          <PaginationLink href={pageButtonUrl(prevEllipsisButtonPageNumber)}>
+            <PaginationEllipsis />
+          </PaginationLink>
+        )}
+
+        {middlePages.map((pageNum) => (
+          <PaginationNumber
+            key={pageNum}
+            pageNumber={pageNum}
+            currentPageNumber={currentPageNumber}
+            pageButtonUrl={pageButtonUrl}
+          />
+        ))}
+
+        {nextEllipsisButtonPageNumber && (
+          <PaginationLink href={pageButtonUrl(nextEllipsisButtonPageNumber)}>
+            <PaginationEllipsis />
+          </PaginationLink>
+        )}
+
+        <PaginationNumber
+          pageNumber={totalPages}
+          currentPageNumber={currentPageNumber}
+          pageButtonUrl={pageButtonUrl}
+        />
+      </PaginationContent>
+    </Pagination>
+  );
+}
+
+const PaginationNumber = ({
+  pageNumber,
+  currentPageNumber,
+  pageButtonUrl,
+}: {
+  pageNumber: number;
+  currentPageNumber: number;
+  pageButtonUrl: (page: number) => string;
+}) => (
+  <PaginationItem>
+    <PaginationLink
+      href={pageButtonUrl(pageNumber)}
+      isActive={pageNumber === currentPageNumber}
+    >
+      {pageNumber}
+    </PaginationLink>
+  </PaginationItem>
+);
