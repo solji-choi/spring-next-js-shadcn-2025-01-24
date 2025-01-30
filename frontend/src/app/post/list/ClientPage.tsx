92374a0a5bef6a6c7c29a56017da22c708522937
solji <solji.choi89@gmail.com>
2025-01-31 05:12:41 +0900

    검색상자에 Card, 반응형 작업

--- frontend/src/app/post/list/ClientPage.tsx
+++ frontend/src/app/post/list/ClientPage.tsx
@@ -7,6 +7,14 @@ import type { components } from "@/lib/backend/apiV1/schema";
 import PaginationType1Responsive from "@/lib/business/components/PaginationType1Responsive";
 
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -37,73 +45,85 @@ export default function ClientPage({
     <div className="container mx-auto px-4">
       <h1 className="text-2xl font-bold text-center my-4">공개글</h1>
 
-      <form
-        className="flex flex-col gap-6"
-        onSubmit={(e) => {
-          e.preventDefault();
+      <Card>
+        <CardHeader>
+          <CardTitle>검색</CardTitle>
+          <CardDescription>검색어와 검색 조건을 입력해주세요.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form
+            className="flex flex-col gap-6 md:flex-row"
+            onSubmit={(e) => {
+              e.preventDefault();
 
-          const formData = new FormData(e.target as HTMLFormElement);
-          const searchKeyword = formData.get("searchKeyword") as string;
-          const searchKeywordType = formData.get("searchKeywordType") as string;
-          const page = formData.get("page") as string;
-          const pageSize = formData.get("pageSize") as string;
+              const formData = new FormData(e.target as HTMLFormElement);
+              const searchKeyword = formData.get("searchKeyword") as string;
+              const searchKeywordType = formData.get(
+                "searchKeywordType",
+              ) as string;
+              const page = formData.get("page") as string;
+              const pageSize = formData.get("pageSize") as string;
 
-          router.push(
-            `?page=${page}&pageSize=${pageSize}&searchKeywordType=${searchKeywordType}&searchKeyword=${searchKeyword}`,
-          );
-        }}
-      >
-        <input type="hidden" name="page" value="1" />
+              router.push(
+                `?page=${page}&pageSize=${pageSize}&searchKeywordType=${searchKeywordType}&searchKeyword=${searchKeyword}`,
+              );
+            }}
+          >
+            <input type="hidden" name="page" value="1" />
 
-        <FormItem>
-          <Label>페이지 당 행 수</Label>
+            <FormItem>
+              <Label>페이지 당 행 수</Label>
 
-          <Select name="pageSize" defaultValue={pageSize.toString()}>
-            <SelectTrigger>
-              <SelectValue placeholder="페이지 당 행 수" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value="10">10개씩 보기</SelectItem>
-                <SelectItem value="30">30개씩 보기</SelectItem>
-                <SelectItem value="50">50개씩 보기</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </FormItem>
+              <Select name="pageSize" defaultValue={pageSize.toString()}>
+                <SelectTrigger>
+                  <SelectValue placeholder="페이지 당 행 수" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectItem value="10">10개씩 보기</SelectItem>
+                    <SelectItem value="30">30개씩 보기</SelectItem>
+                    <SelectItem value="50">50개씩 보기</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </FormItem>
 
-        <FormItem>
-          <Label>검색어 타입</Label>
+            <FormItem>
+              <Label>검색어 타입</Label>
 
-          <Select name="searchKeywordType" defaultValue={searchKeywordType}>
-            <SelectTrigger>
-              <SelectValue placeholder="검색어 타입" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value="title">제목</SelectItem>
-                <SelectItem value="content">내용</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </FormItem>
+              <Select name="searchKeywordType" defaultValue={searchKeywordType}>
+                <SelectTrigger>
+                  <SelectValue placeholder="검색어 타입" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectItem value="title">제목</SelectItem>
+                    <SelectItem value="content">내용</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </FormItem>
 
-        <FormItem>
-          <Label>검색어</Label>
+            <FormItem className="md:flex-grow">
+              <Label>검색어</Label>
 
-          <Input
-            placeholder="검색어를 입력해주세요."
-            type="text"
-            name="searchKeyword"
-            defaultValue={searchKeyword}
-          />
-        </FormItem>
+              <Input
+                placeholder="검색어를 입력해주세요."
+                type="text"
+                name="searchKeyword"
+                defaultValue={searchKeyword}
+              />
+            </FormItem>
 
-        <Button type="submit">검색</Button>
-      </form>
+            <Button type="submit" className="md:self-end">
+              검색
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
 
       <PaginationType1Responsive
-        className="my-2"
+        className="my-6"
         baseQueryString={`pageSize=${pageSize}&searchKeywordType=${searchKeywordType}&searchKeyword=${searchKeyword}`}
         totalPages={itemPage.totalPages}
         currentPageNumber={itemPage.currentPageNumber}
@@ -127,7 +147,7 @@ export default function ClientPage({
       </ul>
 
       <PaginationType1Responsive
-        className="my-2"
+        className="my-6"
         baseQueryString={`pageSize=${pageSize}&searchKeywordType=${searchKeywordType}&searchKeyword=${searchKeyword}`}
         totalPages={itemPage.totalPages}
         currentPageNumber={itemPage.currentPageNumber}
