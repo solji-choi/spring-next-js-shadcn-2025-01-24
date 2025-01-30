bd5a91d4618347f08454e5e37e174ddc72c5d637
solji <solji.choi89@gmail.com>
2025-01-31 03:32:58 +0900

    ellipsis 버튼이 나와야 할 때만 나오게 하고, ellipsis 버튼에도 링크 걸기

--- frontend/src/app/post/list/ClientPage.tsx
+++ frontend/src/app/post/list/ClientPage.tsx
@@ -91,7 +91,18 @@ export default function ClientPage({
               1
             </PaginationLink>
           </PaginationItem>
-          <PaginationEllipsis />
+
+          {itemPage.currentPageNumber - paginationArmSize - 1 > 1 && (
+            <PaginationLink
+              href={`?page=${itemPage.currentPageNumber - paginationArmSize - 1}&pageSize=${pageSize}&searchKeywordType=${searchKeywordType}&searchKeyword=${searchKeyword}`}
+              isActive={
+                itemPage.currentPageNumber - paginationArmSize - 1 ===
+                itemPage.currentPageNumber
+              }
+            >
+              <PaginationEllipsis />
+            </PaginationLink>
+          )}
           {Array.from({ length: itemPage.totalPages }, (_, i) => i + 1)
             .filter(
               (pageNum) =>
@@ -110,7 +121,19 @@ export default function ClientPage({
                 </PaginationLink>
               </PaginationItem>
             ))}
-          <PaginationEllipsis />
+
+          {itemPage.currentPageNumber + paginationArmSize + 1 <
+            itemPage.totalPages && (
+            <PaginationLink
+              href={`?page=${itemPage.currentPageNumber + paginationArmSize + 1}&pageSize=${pageSize}&searchKeywordType=${searchKeywordType}&searchKeyword=${searchKeyword}`}
+              isActive={
+                itemPage.currentPageNumber + paginationArmSize + 1 ===
+                itemPage.currentPageNumber
+              }
+            >
+              <PaginationEllipsis />
+            </PaginationLink>
+          )}
           <PaginationItem>
             <PaginationLink
               href={`?page=${itemPage.totalPages}&pageSize=${pageSize}&searchKeywordType=${searchKeywordType}&searchKeyword=${searchKeyword}`}
